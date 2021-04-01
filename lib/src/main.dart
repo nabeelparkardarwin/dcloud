a085bf4a48bb50fb1c10b18faee81df05e148ef4
Nabeel Parkar <nabeel.parkar@darwinpgc.com>
2021-04-01 14:51:35 +0530

    Added documentation and git and issue links in pubspec.yaml

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -7,9 +7,11 @@ import 'infrustructure/d_cloud_repository.dart';
 class DCloud {
   final IDCloudRepository _repository;
 
+  /// Interface with DCloud. Required valid [apiKey] and [apiSecret]
   DCloud({required String apiKey, required String apiSecret})
       : _repository = DCloudRepository(apiKey: apiKey, apiSecret: apiSecret);
 
+  /// Upload a file to DCloud
   Future<DCloudUploadResult> upload(File file) async {
     return _repository.upload(file);
   }
